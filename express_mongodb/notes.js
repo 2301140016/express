132f3e4071e198029d99a092b5c69b634ea2c1f1
MarikoItoQA <mariko.itou@os.lpixel.net>
2025-12-03 20:01:17 +0900

    Add kadai dog

--- express_mongodb/notes.js
+++ express_mongodb/notes.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 // 接続情報を設定
 const { MongoClient } = require("mongodb");
-const uri = "*****";
+const uri = "mongodb+srv://2301140016yn_db_user:password2301140016@cluster0.nm19rxp.mongodb.net/?appName=Cluster0";
 const client = new MongoClient(uri);
 
 router.get('/', async (req, res) => {
