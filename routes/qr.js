f797f9b52879ea6d3b20573041cc98059ac1ad98
MarikoItoQA <mariko.itou@os.lpixel.net>
2025-12-03 19:34:56 +0900

    Add kadai

--- routes/qr.js
+++ routes/qr.js
@@ -7,7 +7,7 @@ const cors = require('cors'); // corsミドルウェアを追加
 router.use(cors());
 
 router.get('/', async (req, res) => {
-request('https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=https://www.cyber-u.ac.jp/index.html', function (error, response, body) {
+request('https://yesno.wtf/api', function (error, response, body) {
 if (!error && response.statusCode == 200) {
 const data = JSON.parse(body);
 res.json(data);
